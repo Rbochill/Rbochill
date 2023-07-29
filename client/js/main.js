3166352e62bd53ca32bb7e0df5fd1f06c04f4187
kimInDa <pinktail2327@naver.com>
2023-07-30 07:18:20 +0900

    [Add] HTML/CSS 네비게이션 구현 #65 #66 - nav.html 파일을 생성하였습니다. - nav.html이 모든 페이지에서 보이도록 main.js에서 구현합니다. - 네비게이션 메뉴를 스크롤 고정 상태와 움직일 상태를 구분하여 구현합니다.

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,7 @@
 // % 공통 헤더, 공통 푸터
 const header = document.querySelector('header');
 const footer = document.querySelector('footer');
+const nav = document.querySelector('nav');
 
 fetch('./pages/common/header.html')
 	.then((res) => res.text())
@@ -9,3 +10,7 @@ fetch('./pages/common/header.html')
 fetch('./pages/common/footer.html')
 	.then((res) => res.text())
 	.then((data) => (footer.innerHTML = data));
+
+fetch('./pages/common/nav.html')
+  .then((res) => res.text())
+  .then((data) => (nav.innerHTML = data));
