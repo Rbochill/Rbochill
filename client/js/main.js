02edd05816f8e95454c0757a5eede62e4104f4ff
Moong <tmdals9154@gmail.com>
2023-07-30 17:47:30 +0900

    [Add] 공통적으로 사용되는 js 파일 구조화 #82
    공통적으로 헤더와 푸터를 부르는 기능을 common.js로 따로 분류 하였습니다.

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,1 @@
-// % 공통 헤더, 공통 푸터
-const header = document.querySelector('header');
-const footer = document.querySelector('footer');
-
-fetch('/pages/common/header.html')
-	.then((res) => res.text())
-	.then((data) => (header.innerHTML = data));
-
-fetch('/pages/common/footer.html')
-	.then((res) => res.text())
-	.then((data) => (footer.innerHTML = data));
+import {headerFooter} from './common/index.js';
