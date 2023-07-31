7772adafab941ae746fcd5e9976187a374038f1d
haha41 <thgml500@gmail.com>
2023-07-31 09:42:49 +0900

    [Add] Unique ID는 localStorage에 저장하기#76
    회원가입 페이지에서 가입하기 버튼 누르면 unique id 생성되어 로컬스토리지에 저장됨

--- client/js/signUp.js
+++ client/js/signUp.js
@@ -1,11 +1,13 @@
 import {getNode} from '/lib/index.js';
 
 const buttonSignUp = getNode('.buttonSignUp');
+const {localStorage: storage} = globalThis;
 
+// # 랜덤한 문자 생성
 function handleRandom() {
 	const userId = Math.random().toString(36).substring(2, 11);
 
-	return userId;
+	storage.setItem('uniqueId', userId);
 }
 
 buttonSignUp.addEventListener('click', handleRandom);
