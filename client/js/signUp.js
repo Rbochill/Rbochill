dbfd850b97ffcef95af8dd185c61269dd4489f07
haha41 <thgml500@gmail.com>
2023-07-31 18:59:26 +0900

    [Add] 회원가입 구현 # 123, 124, 125, 126
    1. 회원 가입 시 필수 정보 입력 후에만 가입되도록 함
    2. 생성된 unique ID와 입력된 회원 정보를 로컬 스토리지에 전송되도록 함
    3. 회원 가입 후 로그인 페이지로 이동되게 함

--- client/js/signUp.js
+++ client/js/signUp.js
@@ -1,16 +1,83 @@
-import {getNode, tiger} from '/lib/index.js';
+import {getNode, tiger, getNodes} from '/lib/index.js';
+import {emailReg, pwReg} from '/js/common/function.js';
+
+const inputId = getNode('.inputId');
+const inputPw1 = getNode('.inputPw1');
+const inputPw2 = getNode('.inputPw2');
+const inputName = getNode('.inputName');
+const inputEmail = getNode('.inputEmail');
+const inputPhone = getNode('.inputPhone');
+const inputYear = getNode('.inputYear');
+const inputMonth = getNode('.inputMonth');
+const inputDay = getNode('.inputDay');
+const inputGender = getNodes('.inputGender');
 
 const buttonSignUp = getNode('.buttonSignUp');
+const errorMessage1 = getNode('.errorMessage1');
+const errorMessage2 = getNode('.errorMessage2');
 const {localStorage: storage} = globalThis;
-// const URL = 'http://localhost:3000/users'
+// const URL = 'http://localhost:3000/users';
 
-// # 랜덤한 문자 생성
-function handleRandom() {
-	const uniqueId = Math.random().toString(36).substring(2, 11);
+errorMessage1.hidden = true;
+errorMessage2.hidden = true;
 
-	storage.setItem('uniqueId', uniqueId);
-	// tiger post 통신을 통해서 uniqueId를 보낸다.
-	tiger.post(URL, {uniqueId: uniqueId});
+// # 아이디 입력
+function handleCheckId() {
+	const value = this.value;
+	if (!emailReg(value) && value.length !== 0) {
+		errorMessage1.hidden = false;
+	} else if (value.length === 0) {
+		errorMessage1.hidden = true;
+	} else {
+		errorMessage1.hidden = true;
+	}
 }
 
-buttonSignUp.addEventListener('click', handleRandom);
+// # 비밀번호 입력
+function handleCheckPw() {
+	const value = this.value;
+	if (!pwReg(value) && value.length !== 0) {
+		errorMessage2.hidden = false;
+	} else if (value.length === 0) {
+		errorMessage2.hidden = true;
+	} else {
+		errorMessage2.hidden = true;
+	}
+}
+
+// # 가입하기
+function handlesignUp(e) {
+	e.preventDefault();
+
+	const selectedGender = Array.from(inputGender).find((radio) => radio.checked);
+	if (inputId.value !== '' && inputPw1.value !== '' && inputPw2.value !== '' && inputName.value !== '' && inputEmail.value !== '' && inputPhone.value !== '') {
+		const uniqueId = Math.random().toString(36).substring(2, 11);
+		const birthday = `${inputYear.value}-${inputMonth.value}-${inputDay.value}`;
+		storage.setItem('userId', inputId.value);
+		storage.setItem('password', inputPw1.value);
+		storage.setItem('uniqueId', uniqueId);
+		storage.setItem('name', inputName.value);
+		storage.setItem('phoneNum', inputPhone.value);
+		storage.setItem('gender', selectedGender.value);
+		storage.setItem('birthDate', [birthday]);
+
+		window.location.href = '/pages/login.html';
+
+		// 유저 정보 전송
+		tiger.post(URL, {
+			userId: inputId.value,
+			password: inputPw1.value,
+			uniqueId: uniqueId,
+			name: inputName.value,
+			phoneNum: inputPhone.value,
+			gender: '',
+			birthDate: ['', '', ''],
+		});
+	} else {
+		alert('필수입력사항을 기입해주세요.');
+	}
+}
+
+inputId.addEventListener('input', handleCheckId);
+inputPw1.addEventListener('input', handleCheckPw);
+buttonSignUp.addEventListener('click', handlesignUp);
