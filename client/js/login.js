2d040595eb24f4559172c1596d41ddf845aff8f2
haha41 <thgml500@gmail.com>
2023-08-01 12:18:29 +0900

    [Add] 로그인 시 서버와 로컬의 데이터 비교
    서버의 uniqueID와 로컬스토리지의 uniqueID를 비교하여 일치 시 로그인 되도록 함

--- client/js/login.js
+++ client/js/login.js
@@ -2,15 +2,11 @@
 import {getNode} from '/lib/index.js';
 import {emailReg, pwReg} from '/js/common/function.js';
 
-// const user = {
-// 	id: 'asd@naver.com',
-// 	pw: 'spdlqj123!@',
-// };
-
 const inputId = getNode('.inputId');
 const inputPw = getNode('.inputPw');
 const buttonLogin = getNode('.buttonLogin');
 const buttonSignUp = getNode('.buttonSignUp');
+const {localStorage: storage} = globalThis;
 
 let emailPass = false;
 let pwPass = false;
@@ -45,18 +41,34 @@ let user = {};
 
 let response = await fetch('http://localhost:3000/users');
 user = await response.json();
+// 서버에서 가져온 데이터
+console.log('id :', user[0].userId);
+console.log(('pw :', user[0].password));
 
 function handleLogin(e) {
 	e.preventDefault();
-
+	//  사용자가 입력한 데이터
 	const id = inputId.value;
 	const pw = inputPw.value;
+	//  서버에서 가져온 데이터
 	let getUserId = user[0].userId;
 	let getUserPw = user[0].password;
-	if (id === getUserId && pw === getUserPw) {
-		alert(user[0].uniqeId);
 
-		window.location.href = '/index.html';
+	if (id === getUserId && pw === getUserPw) {
+		// 서버의 데이터를 로컬 스토리지에 저장
+		const serverUniqueId = user[0].uniqueId;
+		storage.setItem('uniqueId', serverUniqueId);
+
+		// 로컬 스토리지의 데이터 유니크 아이디 가져오기
+		const localUniqueId = storage.getItem('uniqueId');
+
+		console.log('localUniqueId :', localUniqueId);
+		console.log('uniqeId :', user[0].uniqueId);
+
+		if (serverUniqueId === localUniqueId) {
+			// console.log('성공');
+			window.location.href = '/index.html';
+		}
 	} else {
 		alert('정확한 아이디와 비밀번호를 입력해 주세요.');
 	}
